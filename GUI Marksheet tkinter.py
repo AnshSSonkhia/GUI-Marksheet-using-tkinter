ce509b962ca3a76b68450346e3a3e4eea86a8de0
ANSH S SONKHIA <72140083+AnshSSonkhia@users.noreply.github.com>
2021-10-03 18:21:38 +0530

    Create GUI Marksheet tkinter.py

---
+++ GUI Marksheet tkinter.py
@@ -0,0 +1,15 @@
+NumList=[]
+evenSum=0
+
+Number=int(input("Please enter the total number of list elements: "))
+for i in range(1, Number+1):
+    value=int(input("Please enter the value of %d Elements: " %i))
+    
+Sum of even numbers from a list
+
+
+NumList.append(value)
+for j in range(Number):
+    if(NumList[j]%2==0):
+        evenSum=evenSum+NumList[j]
+print("\n The sum of even numbers in the list= ",evenSum)
